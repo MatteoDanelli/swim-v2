3c7a53629affd075e73ce6830217e14c680050a6
roachteo <roachteo@30c696eb-23b9-41e5-13da-83aea6c56d46>
2012-12-23 14:25:47 +0000

    Added.

---
+++ Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreAdmin.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package SE2.Swimv2.Session;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import SE2.Swimv2.Entity.Admin;
+
+/**
+ * @author Matteo Danelli
+ * Implementazione dell'interfaccia GestoreAdminRemote.
+ */
+@Stateless
+public class GestoreAdmin implements GestoreAdminRemote {
+	@PersistenceContext(unitName = "Swimv2")
+	EntityManager database;
+
+	//Crea l'admin con la mail e la password dati come parametri
+	@Override
+	public void createAdmin(String email, String password) {
+		try {
+			database.persist(new Admin(email, password));
+		}
+		catch (EntityExistsException e)
+		{
+			//Admin già presente! Deve essere unico, per cui non dovrebbe mai arrivare qui!
+		}
+	}
+
+	//Consente la modifica della password dell'admin
+	@Override
+	public void modificaPassword(String nuovaPassword) {
+		}
+
+}
