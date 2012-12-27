d0ff856e642557420021a134125f8ebb128c5921
daniel.cantoni90@gmail.com <daniel.cantoni90@gmail.com@30c696eb-23b9-41e5-13da-83aea6c56d46>
2012-12-27 23:40:10 +0000

    risolto problema relazione amici( molti a molti su tabella user)

--- Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreAmici.java
+++ Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreAmici.java
@@ -1,11 +1,13 @@
 package SE2.Swimv2.Session;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import SE2.Swimv2.Entity.Feedback;
 import SE2.Swimv2.Entity.User;
 
 /**
@@ -21,23 +23,32 @@ public class GestoreAmici implements GestoreAmiciRemote {
     public GestoreAmici() {
         // TODO Auto-generated constructor stub
     }
-
 	@Override
-	public void aggiungiAmicizia(User user1, User user2) {
-		// TODO Auto-generated method stub
+	public void aggiungiAmicizia(long idUser1, long idUser2) {
+		User user1 = database.find(User.class, idUser1);
+		User user2 = database.find(User.class, idUser2);
 		
+		user1.addAmico(user2);
 	}
-
+	
 	@Override
-	public Set<User> elencoAmici(User user) {
+	public Set<User> elencoAmici(long idUser) {
+		User user = database.find(User.class, idUser);
+		Set<User> set = new HashSet<User>();
+		for(User u: user.getAmici1()){
+			set.add(u);
+		}
+		for(User u: user.getAmici2()){
+			set.add(u);
+		}
+		return set;
+	}
+	@Override
+	public Boolean verificaAmicizia(long idUser1, long idUser2) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public Boolean verificaAmicizia(User user1, User user2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 
 }
