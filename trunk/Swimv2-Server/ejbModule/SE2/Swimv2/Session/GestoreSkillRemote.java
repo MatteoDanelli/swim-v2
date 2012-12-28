6cae5caf17e0fb2b44fb05146ff19493ff2e7d4f
roachteo <roachteo@30c696eb-23b9-41e5-13da-83aea6c56d46>
2012-12-28 00:29:05 +0000

    Inziato anche gestore Skill. Non finito e non testato

--- trunk/Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreSkillRemote.java
+++ trunk/Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreSkillRemote.java
@@ -3,8 +3,7 @@
  */
 package SE2.Swimv2.Session;
 
-import java.util.Set;
-
+import java.util.List;
 import javax.ejb.Remote;
 
 import SE2.Swimv2.Entity.Skill;
@@ -12,10 +11,11 @@ import SE2.Swimv2.Entity.User;
 
 /**
  * @author Matteo Danelli
- * Interfaccia per la gestione dell'entità Skill
+ * Interfaccia per la gestione delle Skill.
  */
 @Remote
 public interface GestoreSkillRemote {
-	Set<Skill> getSkillSet();
-	Set<Skill> getPersonalSkill(User user);
+	void createSkill(String nome);
+	List<Skill> getSkillSet();
+	List<Skill> getPersonalSkill(User user);
 }
--- trunk/Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreUser.java
+++ trunk/Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreUser.java
@@ -2,19 +2,17 @@ package SE2.Swimv2.Session;
 
 import java.util.Date;
 import java.util.Set;
-import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceException;
-
 
 import SE2.Swimv2.Entity.Skill;
 import SE2.Swimv2.Entity.User;
 
 /**
+ * @author Daniel Cantoni, Matteo Danelli
  * Session Bean che implementa l'interfaccia GestoreUserRemote.
  */
 @Stateless
@@ -23,19 +21,18 @@ public class GestoreUser implements GestoreUserRemote {
 	EntityManager database;
 	
     /**
-     * Inserisce un nuovo utente 
+     * Inserisce un nuovo utente, assegnandoli tutti i suoi attributi.
      */
 	@Override
-	public User addUser(String email, String password, String nome,
-			String cognome, String provincia, char sesso, Date datanascita,
+	public User addUser(String email, String password, String nome,String cognome, String provincia, char sesso, Date datanascita,
 			Set<Skill> personalSkill) {
-			User u = new User(email, password, nome, cognome, provincia, sesso, datanascita);
+			User newUser = new User(email, password, nome, cognome, provincia, sesso, datanascita);
     	try{
-    		database.persist(u);
+    		database.persist(newUser);
     	}catch(EntityExistsException e){
     		//Entità già presente. Non verrà inserita ma la sua id verrà saltata.
     	}
-		return u;
+		return newUser;
 	}
 
 	/**
