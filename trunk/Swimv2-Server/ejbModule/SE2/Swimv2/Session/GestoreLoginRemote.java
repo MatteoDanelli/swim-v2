7950a935808e07a6ee8ba736673f717c243dd692
roachteo@gmail.com <roachteo@gmail.com@30c696eb-23b9-41e5-13da-83aea6c56d46>
2012-12-30 15:08:51 +0000

    Fix gestoreLogin con eccezione corretta

--- trunk/Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreLoginRemote.java
+++ trunk/Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreLoginRemote.java
@@ -2,6 +2,8 @@ package SE2.Swimv2.Session;
 
 import javax.ejb.Remote;
 
+import SE2.Swimv2.Exceptions.LoginException;
+
 /**
  * @author Daniel Cantoni
  * Interfaccia per la gestione dei login, per gli user o per l'admin.
@@ -9,6 +11,6 @@ import javax.ejb.Remote;
 @Remote
 public interface GestoreLoginRemote {
 
-	public long loginUser(String email, String password);
-	public long loginAdmin(String email, String password);
+	public long loginUser(String email, String password) throws LoginException;
+	public long loginAdmin(String email, String password) throws LoginException;
 }
