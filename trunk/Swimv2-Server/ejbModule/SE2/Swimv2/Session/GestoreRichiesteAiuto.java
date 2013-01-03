ac4a2e270af54b5c19d2babce4bc3cd3f023dedb
roachteo@gmail.com <roachteo@gmail.com@30c696eb-23b9-41e5-13da-83aea6c56d46>
2013-01-03 20:33:45 +0000

    Create GestoreRichiesteAiuto. WARNING: che pessima scelta di design che abbiamo fatto. E' una copia identica -eccetto una riga- di GestoreMessaggi.

--- trunk/Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreRichiesteAiuto.java
+++ trunk/Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreRichiesteAiuto.java
@@ -1,13 +1,19 @@
 package SE2.Swimv2.Session;
 
-import java.util.Set;
+import java.util.Date;
+import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import SE2.Swimv2.Entity.Messaggio;
 import SE2.Swimv2.Entity.User;
+import SE2.Swimv2.Exceptions.MessaggiException;
 
 /**
  * Session Bean implementation class GestoreRichiesteAiuto
@@ -25,34 +31,60 @@ public class GestoreRichiesteAiuto implements GestoreRichiesteAiutoRemote {
     }
 
 	@Override
-	public void inviaRichiestaAiuto(User mittente, User destinatario,
-			String testo) {
-		// TODO Auto-generated method stub
-		
+	public void inviaRichiestaAiuto(long mittente, long destinatario, String testo) throws MessaggiException {
+		if (mittente != destinatario) {
+			Messaggio nuovoMessaggio = new Messaggio();
+			User userMittente = database.find(User.class, mittente);
+			User UserDestinatario = database.find(User.class, destinatario);
+			nuovoMessaggio.setMittente(userMittente);
+			nuovoMessaggio.setDestinatario(UserDestinatario);
+			nuovoMessaggio.setTesto(testo);
+			nuovoMessaggio.setDataInvio(new Date());
+			nuovoMessaggio.setRichiestaAiuto(true);
+			database.persist(nuovoMessaggio);
+		}
+		else throw new MessaggiException("Mittente e destinario coincidenti!");
+	}
+
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Messaggio> elencoRichiesteAiuto(long user) throws MessaggiException {
+		User userCercato = database.find(User.class, user);
+		Query q = database.createQuery("FROM Messaggio m WHERE m.destinatario=:userDestinatario ORDER BY m.dataInvio desc, m.isMessaggioLetto desc");
+		q.setParameter("userDestinatario", userCercato);
+		try {
+			List<Messaggio> elenco = (List<Messaggio>) q.getResultList();
+			return elenco;
+		} catch (EntityNotFoundException e) {
+		} catch (NoResultException e) {
+		} catch (NonUniqueResultException e) {
+		}
+		throw new MessaggiException("Non esistono messaggi!");
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Boolean verificaNuoveRichiesteAiuto(long user) {
+		User userCercato = database.find(User.class, user);
+		Query q = database.createQuery("FROM Messaggio m WHERE m.destinatario=:userDestinatario AND m.isMessaggioLetto='false'");
+		q.setParameter("userDestinatario", userCercato);
+		List<Messaggio> elenco = (List<Messaggio>) q.getResultList();
+		if (elenco.size()!=0)
+			return true;
+		return false;
 	}
 
 	@Override
-	public Set<Messaggio> RichiesteAiuto(User user) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Boolean verificaNuoveRichiesteAiuto(User user) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void settaRichiestaLetta(Messaggio messaggio) {
-		// TODO Auto-generated method stub
-		
+	public void settaRichiestaLetta(long messaggio) {
+		Messaggio messaggioLetto = database.find(Messaggio.class, messaggio);
+		messaggioLetto.setMessaggioLetto(true);		
 	}
 
 	@Override
 	public Messaggio getById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Messaggio messaggioCercato = database.find(Messaggio.class, id);
+		return messaggioCercato;
 	}
 
 }
