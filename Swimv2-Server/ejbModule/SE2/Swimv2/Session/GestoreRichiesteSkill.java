a7b93a831dc64a6725c165dd4f1ec9b2c07ec500
roachteo@gmail.com <roachteo@gmail.com@30c696eb-23b9-41e5-13da-83aea6c56d46>
2013-01-11 21:39:12 +0000

    Aggiunta gestione eccezioni gestoreRichieste

--- Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreRichiesteSkill.java
+++ Swimv2-Server/ejbModule/SE2/Swimv2/Session/GestoreRichiesteSkill.java
@@ -5,11 +5,13 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
 import javax.persistence.Query;
 
 import SE2.Swimv2.Entity.RichiestaSkill;
 import SE2.Swimv2.Entity.Skill;
 import SE2.Swimv2.Entity.User;
+import SE2.Swimv2.Exceptions.RichiesteSkillException;
 
 /**
  * Session Bean implementation class GestoreRichiesteSkill
@@ -34,23 +36,39 @@ public class GestoreRichiesteSkill implements GestoreRichiesteSkillRemote {
 	}
 
 	@Override
-	public void accettaRichiesta(RichiestaSkill skillDaAccettare) {
+	public void accettaRichiesta(RichiestaSkill skillDaAccettare) throws RichiesteSkillException {
+		try {
 		database.remove(skillDaAccettare);
 		String nomeSkill = skillDaAccettare.getSkillRichiesta();
 		Skill nuovaSkill = new Skill(nomeSkill);
 		database.persist(nuovaSkill);
+		}
+		catch (Exception e) {
+			throw new RichiesteSkillException("Errore nell'accettare la richiesta!");
+		}
 	}
 
 	@Override
-	public void rifiutaRichiesta(RichiestaSkill skillDaRifiutare) {
+	public void rifiutaRichiesta(RichiestaSkill skillDaRifiutare) throws RichiesteSkillException {
+		try {
 		database.remove(skillDaRifiutare);
+		}
+		catch (PersistenceException e) {
+			throw new RichiesteSkillException("Errore nel rifiuto della skill!");
+		}
 	}
 
 	@Override
-	public void inviaRichiestaAggiuntaSkill(long userCheLaRichiede,	String skillRichiesta) {
-		User userMittente = database.find(User.class, userCheLaRichiede);
-		RichiestaSkill nuovaRichiesta = new RichiestaSkill(userMittente, skillRichiesta);
-		database.persist(nuovaRichiesta);		
+	public void inviaRichiestaAggiuntaSkill(long userCheLaRichiede,	String skillRichiesta) throws RichiesteSkillException {
+		try {
+			User userMittente = database.find(User.class, userCheLaRichiede);
+			RichiestaSkill nuovaRichiesta = new RichiestaSkill(userMittente, skillRichiesta);
+			database.persist(nuovaRichiesta);	
+		}
+		catch (Exception e) {
+			throw new RichiesteSkillException("Errore nell'invio della richiesta!");
+		}
+	
 	}
 	
 	@SuppressWarnings("unchecked")
